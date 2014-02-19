4691e4b5ffebf1e4b77c5020aab720ca2de0659e
robbie <robbie.perlstein@gmail.com>
2014-02-19 00:59:25 -0500

    Styled and added menu

--- views/menu.php
+++ views/menu.php
@@ -1,7 +1,7 @@
 <div id="menu">
   <ul >
-    <li>Home</li>
-    <li>Blog</li>
-    <li>Resume</li>
+    <li><a href="http://robbiep.com">Home</a></li>
+    <li><a href="http://growcode.blogspot.com/">Blog</a></li>
+    <li><a href="assets/resume.pdf">Resume</a></li>
   </ul>
 </div>
