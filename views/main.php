7d60e57320199d2562d2ef13877693944cfab1d0
robbie <rperlste@gmail.com>
2014-02-25 21:32:50 -0700

    Restyled theme and updated content section

--- views/main.php
+++ views/main.php
@@ -9,14 +9,14 @@
       <img id="my_pic" src="<?php $_SERVER["DOCUMENT_ROOT"] ?>/assets/mypic.jpg">
     </div>
     <div id="column_right">
-      I'll be graduating from CU Denver in May 2014, and I'm completely
+      I'll be graduating from CU Denver in May 2014, and I'm very
       excited to make the leap into a rewarding career of solving complex
       problems and writing kick ass software.
       <br><br>
       I have worked on trading systems and government regulatory compliance
-      applications for derivatives market broker dealers. I have done front-end
-      and back-end development for a powerful social utility application used
-      at 100s of conferences and events around the world.
+      applications for derivatives market broker dealers. I have done back-end
+      and front-end development for a powerful social networking application
+      used at 100s of conferences and events around the world.
       <br><br>
       I maintain a 3.85 GPA. I'm currently employed, with 1.5 years development
       exerience under my belt. And sometimes, when the stars align just
