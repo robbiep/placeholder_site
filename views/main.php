63f46c7f54c95633397d48458cf6dc1aa54bebef
robbie <robbie.perlstein@gmail.com>
2014-03-01 17:20:58 -0500

    Updated some typos on the main page

--- views/main.php
+++ views/main.php
@@ -8,18 +8,17 @@
       <img id="my_pic" src="<?php $_SERVER["DOCUMENT_ROOT"] ?>/assets/mypic.jpg">
     </div>
     <div id="column_right">
-      This coming May I will be graduating from CU Denver and I'm very
-      excited to make the leap into a rewarding career of solving complex
-      problems and writing kick ass software.
+      This May I will be graduating from CU Denver with a degree in
+      computer science, and I'm ready to make the leap into a rewarding 
+      career of solving complex problems and writing kick ass software.
       <br><br>
       I have worked on trading systems and government regulatory compliance
       applications for derivatives market broker dealers. I have done back-end
       and front-end development for a powerful social networking application
-      used at 100s of conferences and events around the world.
+      used at hundreds of conferences and events around the world.
       <br><br>
-      I maintain a 3.85 GPA. I'm currently employed, with 1.5 years development
+      I maintain a 3.85 GPA. I have 1.5 years of development
       experience under my belt. And sometimes, when the stars align just
-      right, I get a chance to go snowboarding or even hangout with my friends
-      too.
+      right, I get to hangout with my friends.
     </div>
   </div>
