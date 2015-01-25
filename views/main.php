499f5bb2ef633b52273d891c01799fd4dc75ab12
robbie <robbie.perlstein@gmail.com>
2015-01-24 21:31:48 -0500

    New hero image added, removed text about college since it's been 8 months...

--- views/main.php
+++ views/main.php
@@ -1,24 +1,7 @@
-
+  <img src="/assets/robbie_hero_large.jpg" style="width:100%">
   <div class="container">
     <div id="title">
       <h1>Hello, I'm Robbie Perlstein.</h1>
       <h2>I develop software and it's a lot of fun.</h2>
-    </div>
-    <div id="column_left">
-      <img id="my_pic" src="<?php $_SERVER["DOCUMENT_ROOT"] ?>/assets/mypic.jpg">
-    </div>
-    <div id="column_right">
-      This May I will be graduating from CU Denver with a degree in
-      computer science, and I'm ready to make the leap into a rewarding 
-      career of solving complex problems and writing kick ass software.
-      <br><br>
-      I have worked on trading systems and government regulatory compliance
-      applications for derivatives market broker dealers. I have done back-end
-      and front-end development for a powerful social networking application
-      used at hundreds of conferences and events around the world.
-      <br><br>
-      I maintain a 3.85 GPA. I have 1.5 years of development
-      experience under my belt. And sometimes, when the stars align just
-      right, I get to hangout with my friends.
     </div>
   </div>
