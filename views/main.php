2fba873e936b52d24fa2af9dd5c9735da7377b79
robbie <robbie.perlstein@gmail.com>
2015-02-07 18:44:18 -0500

    Site updates to clarify my intent

--- views/main.php
+++ views/main.php
@@ -2,6 +2,18 @@
   <div class="container">
     <div id="title">
       <h1>Hello, I'm Robbie Perlstein.</h1>
-      <h2>I develop software and it's a lot of fun.</h2>
     </div>
+      <p>I write code in archaic languages like Perl.</p>
+      <p>I used to write archaic languages like Java and C++.</p>
+      <p>I also used to write sexy new languages like Ruby (on Rails).</p>
+      <p>Language doesn't matter.</p>
+      <p>Attitude is the only thing that does.</p>
+      <p>More important than attitude is aptitude for success.</p>
+      <br>
+      <p>I constantly aim to add value to all activities I participate in.
+         This takes the form of joining the business discussion, reviewing
+         architectural decisions within the code, and most importantly a constant barrage of humor (or at least attempts to be funny).
+      </p>
+      <br>
+      <p>I'm eager to hear what you have to say, so let's chat!</p>
   </div>
