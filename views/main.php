242618c8eee76b85390adb1b2cdb37f82a1a7e22
robbie <robbie.perlstein@gmail.com>
2015-02-13 21:35:45 -0500

    Copy updates

--- views/main.php
+++ views/main.php
@@ -4,16 +4,8 @@
       <h1>Hello, I'm Robbie Perlstein.</h1>
     </div>
       <p>I write code in archaic languages like Perl.</p>
-      <p>I used to write archaic languages like Java and C++.</p>
-      <p>I also used to write sexy new languages like Ruby (on Rails).</p>
-      <p>Language doesn't matter.</p>
-      <p>Attitude is the only thing that does.</p>
-      <p>More important than attitude is aptitude for success.</p>
-      <br>
-      <p>I constantly aim to add value to all activities I participate in.
-         This takes the form of joining the business discussion, reviewing
-         architectural decisions within the code, and most importantly a constant barrage of humor (or at least attempts to be funny).
-      </p>
-      <br>
-      <p>I'm eager to hear what you have to say, so let's chat!</p>
+      <p>I used to write in archaic languages like Java and C++.</p>
+      <p>I also used to write in sexy new languages like Ruby (on Rails).</p>
+      <p>Language doesn't really matter.</p>
+      <p>Only attitude and aptitude matter.</p>
   </div>
